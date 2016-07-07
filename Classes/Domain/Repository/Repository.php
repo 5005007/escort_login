e09c03bde00be871bf3b1ec2d82c5167f692c421
MaximKashapov <gkmr.tj@gmail.com>
2016-07-07 17:27:23 +0300

    Version 0.1

---
+++ Classes/Domain/Repository/Repository.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MaximKashapov
+ * Date: 07.07.2016
+ * Time: 10:58
+ */
+class Tx_Escort_Login_Repository
+    extends \TYPO3\CMS\Extbase\Persistence\Repository {}
---
+++ ext_tables.sql
@@ -0,0 +1,10 @@
+CREATE TABLE tx_escort_account (
+        uid int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
+        pid int(11) DEFAULT '0' NOT NULL,
+
+        user varchar(255) DEFAULT 'user' NOT NULL,
+        pass varchar(255) DEFAULT 'pass' NOT NULL,
+
+        PRIMARY KEY (uid),
+        KEY parent (pid)
+);
